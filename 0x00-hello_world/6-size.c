235a0cb0dfcfbec3d41062e7e318e7e1845c2fc7
Toliva <tisopar@gmail.com>
2022-04-07 10:48:13 -0700

    add program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Prints the size of various types on the computer it is compiled and run on.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %u\nSize of an int: %u\nSize of a long int: %u
+			\nSize of a long long int: %u\nSize of a float: %u
+			\n", size0f(char), size0f(int), size0f(long int),
+			size0f(long long int), size0f(float));
+	return (0);
+}
