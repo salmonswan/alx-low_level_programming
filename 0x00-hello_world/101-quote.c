8eb9243ac79506fca230e0f993801f12adac8602
Toliva <tisopar@gmail.com>
2022-04-07 13:04:20 -0700

    add program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * rints exactly and that piece of art is useful\" - Dora Korpar,
+ * 2015-10-19, followed by a new line, to the standard error.
+ * Return: 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
