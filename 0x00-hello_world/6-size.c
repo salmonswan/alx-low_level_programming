4f9d0fedf6af8db7a8a3923efcd0e96100c9d753
Toliva <tisopar@gmail.com>
2022-04-07 12:22:33 -0700

    update script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("\nSize of an int: %u byte(s)", sizeof(int));
 	printf("\nSize of a long int: %u byte(s)", sizeof(long int));
 	printf("\nSize of a long long int: %u byte(s)", sizeof(long long int));
-	printf("\nSize of a float: %u byte(s)", sizeof(float), "\n");
+	printf("\nSize of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
