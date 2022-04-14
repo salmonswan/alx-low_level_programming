23ecd5117cb06e6839865f86d5e4acfa0c47d2a3
Toliva <tisopar@gmail.com>
2022-04-14 10:05:06 -0700

    update program

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - Tests that the function positive_or_negative() gives the correct
- * output when given a case of 0.
- * 
+ * main - tests function that prints if integer is positive or negative 
  * Return: 0
  */
 
