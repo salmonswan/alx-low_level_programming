a34e2566818db3f482d11edc67be41702a93e4e3
Toliva <tisopar@gmail.com>
2022-04-20 15:20:15 -0700

    add program

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints half of a string, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
