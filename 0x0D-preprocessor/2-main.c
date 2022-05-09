ca2799cba8053b7edb1be456a54a293e5ed2a095
Toliva <tisopar@gmail.com>
2022-05-09 00:59:08 -0700

    add file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of the file from where the program is copied
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
